85a1699a1c76d7289f35f8d1d5cc41dd36f9057c
Edina Ilyes <edina.ilyes@lego.com>
2024-02-26 23:06:30 +0100

    terrafrom fmt <path> for formating

--- terraform-labs/section02/variables.tf
+++ terraform-labs/section02/variables.tf
@@ -1,26 +1,26 @@
 variable "aws_region" {
-type = string
-default = "eu-west-1" 
+  type    = string
+  default = "eu-west-1"
 }
 variable "vpc_name" {
-type = string
-default = "demo_vpc"
+  type    = string
+  default = "demo_vpc"
 }
 variable "vpc_cidr" {
-type = string
-default = "10.0.0.0/16"
+  type    = string
+  default = "10.0.0.0/16"
 }
 variable "demo_private_subnets" {
-default = {
-"demo_private_subnet_1" = 1
-"demo_private_subnet_2" = 2
-"demo_private_subnet_3" = 3
-}
+  default = {
+    "demo_private_subnet_1" = 1
+    "demo_private_subnet_2" = 2
+    "demo_private_subnet_3" = 3
+  }
 }
 variable "demo_public_subnets" {
-default = {
-"demo_public_subnet_1" = 1
-"demo_public_subnet_2" = 2
-"demo_public_subnet_3" = 3
-}
+  default = {
+    "demo_public_subnet_1" = 1
+    "demo_public_subnet_2" = 2
+    "demo_public_subnet_3" = 3
+  }
 }
