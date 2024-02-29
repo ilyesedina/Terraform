01fee1f82031ed40344fc129d75171568d5b33bb
Edina Ilyes <edina.ilyes@lego.com>
2024-02-29 09:26:52 +0100

    fixing all

--- terraform_labs/section02/main.tf
+++ terraform_labs/section02/main.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
 #Retrieve the list of AZs in the current AWS region
@@ -8,7 +8,7 @@ data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
 
 #Define the VPC
-resource "aws_vpc" "demo_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
 
   tags = {
@@ -19,9 +19,9 @@ resource "aws_vpc" "demo_vpc" {
 }
 
 #Deploy the private subnets
-resource "aws_subnet" "demo_private_subnets" {
-  for_each          = var.demo_private_subnets
-  vpc_id            = aws_vpc.demo_vpc.id
+resource "aws_subnet" "private_subnets" {
+  for_each          = var.private_subnets
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
   availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
 
@@ -32,9 +32,9 @@ resource "aws_subnet" "demo_private_subnets" {
 }
 
 #Deploy the public subnets
-resource "aws_subnet" "demo_public_subnets" {
-  for_each                = var.demo_public_subnets
-  vpc_id                  = aws_vpc.demo_vpc.id
+resource "aws_subnet" "public_subnets" {
+  for_each                = var.public_subnets
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
   availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
   map_public_ip_on_launch = true
@@ -47,11 +47,11 @@ resource "aws_subnet" "demo_public_subnets" {
 
 #Create route tables for public and private subnets
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.demo_vpc.id
+  vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway.id
+    cidr_block     = "0.0.0.0/0"
+    gateway_id     = aws_internet_gateway.internet_gateway.id
     #nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
   tags = {
@@ -61,10 +61,10 @@ resource "aws_route_table" "public_route_table" {
 }
 
 resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.demo_vpc.id
+  vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     # gateway_id     = aws_internet_gateway.internet_gateway.id
     nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
@@ -76,22 +76,22 @@ resource "aws_route_table" "private_route_table" {
 
 #Create route table associations
 resource "aws_route_table_association" "public" {
-  depends_on     = [aws_subnet.demo_public_subnets]
+  depends_on     = [aws_subnet.public_subnets]
   route_table_id = aws_route_table.public_route_table.id
-  for_each       = aws_subnet.demo_public_subnets
+  for_each       = aws_subnet.public_subnets
   subnet_id      = each.value.id
 }
 
 resource "aws_route_table_association" "private" {
-  depends_on     = [aws_subnet.demo_private_subnets]
+  depends_on     = [aws_subnet.private_subnets]
   route_table_id = aws_route_table.private_route_table.id
-  for_each       = aws_subnet.demo_private_subnets
+  for_each       = aws_subnet.private_subnets
   subnet_id      = each.value.id
 }
 
 #Create Internet Gateway
 resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.demo_vpc.id
+  vpc_id = aws_vpc.vpc.id
   tags = {
     Name = "demo_igw"
   }
@@ -108,9 +108,9 @@ resource "aws_eip" "nat_gateway_eip" {
 
 #Create NAT Gateway
 resource "aws_nat_gateway" "nat_gateway" {
-  depends_on    = [aws_subnet.demo_public_subnets]
+  depends_on    = [aws_subnet.public_subnets]
   allocation_id = aws_eip.nat_gateway_eip.id
-  subnet_id     = aws_subnet.demo_public_subnets["public_subnet_1"].id
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
   tags = {
     Name = "demo_nat_gateway"
   }
