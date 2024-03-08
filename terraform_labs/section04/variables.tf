c9be6bff2b694e0bd5ef5fd13c63099618c8db96
ilyesedina <ilyesedina10@gmail.com>
2024-03-08 14:35:05 +0100

    set default value to a variable

--- terraform_labs/section04/variables.tf
+++ terraform_labs/section04/variables.tf
@@ -38,14 +38,17 @@ variable "aws_access_key" {
 variable "variables_sub_cidr" {
   description = "CIDR Block for the Variables Subnet"
   type        = string
+  default     = "10.0.202.0/24"
 }
 
 variable "variables_sub_az" {
   description = "Availability Zone used Variables Subnet"
   type        = string
+  default     = "eu-west-1a"
 }
 
 variable "variables_sub_auto_ip" {
   description = "Set Automatic IP Assigment for Variables Subnet"
   type        = bool
+  default     = "true"
 }
