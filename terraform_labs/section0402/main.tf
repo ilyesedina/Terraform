acba0fd65520e6036ae499b3147372f6e52842a3
Edina Ilyes <edina.ilyes@lego.com>
2024-03-15 13:57:42 +0100

    Add Output Block to Export Attributes of Resources with static values

--- terraform_labs/section0402/main.tf
+++ terraform_labs/section0402/main.tf
@@ -1,21 +1,23 @@
 # Creating Modules 
- module "subnet_addrs" {
+module "subnet_addrs" {
   source  = "hashicorp/subnets/cidr"
   version = "1.0.0"
-  
+
   base_cidr_block = "10.0.0.0/22"
   networks = [
-  {
-    name     = "module_network_a"
-    new_bits = 2
-  },
-  {
-    name     = "module_network_b"
-    new_bits = 2
-  },
- ]
+    {
+      name     = "module_network_a"
+      new_bits = 2
+    },
+    {
+      name     = "module_network_b"
+      new_bits = 2
+    },
+  ]
 }
 
-output "subnet_addrs" {
-  value = module.subnet_addrs.network_cidr_blocks
+#Declared resource
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr #for this you have to declare the 'vpc_cidr' variable in the variables.tf file  
+  // cidr_block = "10.0.0.0/16" (This is the static way to declare the cidr block)
 }
--- terraform_labs/section0402/variables.tf
+++ terraform_labs/section0402/variables.tf
@@ -5,3 +5,9 @@ variable "aws_secret_access_key" {
 variable "aws_access_key" {
   type = string
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
