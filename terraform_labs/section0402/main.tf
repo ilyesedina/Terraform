4c9915f4c8900a13dcad10165c64a3ee9690bc6a
Edina Ilyes <edina.ilyes@lego.com>
2024-03-15 21:50:32 +0100

    output dynamic data

--- terraform_labs/section0402/main.tf
+++ terraform_labs/section0402/main.tf
@@ -20,4 +20,49 @@ module "subnet_addrs" {
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr #for this you have to declare the 'vpc_cidr' variable in the variables.tf file  
   // cidr_block = "10.0.0.0/16" (This is the static way to declare the cidr block)
+
+  tags = {
+    Name        = var.vpc_name
+    Environment = "demo_environment"
+    Terraform   = "true"
+    #Region      = data.aws_region.current.name
+  }
+}
+
+# Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+#Deploy the public subnets
+resource "aws_subnet" "public_subnets" {
+  for_each   = var.public_subnets
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
+  #availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
+}
+
+# Terraform Resource Block - To Build EC2 instance in Public Subnet
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  // other configuration...
 }
--- terraform_labs/section0402/variables.tf
+++ terraform_labs/section0402/variables.tf
@@ -11,3 +11,15 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "vpc_name" {
+  type    = string
+  default = "demo_vpc"
+}
+
+variable "public_subnets" {
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+  }
+}
