0f45bf6ad3fe972d7a3d1206a129c0e90cb5f8e2
Edina Ilyes <edina.ilyes@lego.com>
2024-03-20 17:49:53 +0100

    multiple providers added

---
+++ terraform_labs/section0402/providers/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.4"
+    }
+    # find version: https://registry.terraform.io/providers/hashicorp/http/latest
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4.2"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.1"
+    }
+  } 
+}
