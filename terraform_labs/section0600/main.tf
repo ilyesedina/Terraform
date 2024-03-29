42b33c7f793b8d732d71fc03d9411ad6bf6d5f2c
Edina Ilyes <edina.ilyes@lego.com>
2024-03-29 12:06:31 +0100

    Public Module Registry

--- terraform_labs/section0600/main.tf
+++ terraform_labs/section0600/main.tf
@@ -409,3 +409,31 @@ output "public_ip_server_subnet_1" {
 output "public_dns_server_subnet_1" {
   value = module.server_subnet_1.public_dns
 }
+
+# AWS Autoscaling module 
+# AWS Ui ->Region -> EC2 -> Auto Scaling Groups -> myasg has been created
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "4.9.0"
+
+  # Autoscaling group
+  name = "myasg"
+
+  vpc_zone_identifier = [aws_subnet.private_subnets["private_subnet_1"].id, 
+  aws_subnet.private_subnets["private_subnet_2"].id] 
+  #aws_subnet.private_subnets["private_subnet_3"].id
+  min_size            = 0
+  max_size            = 1
+  desired_capacity    = 1
+
+  # Launch template
+  use_lt    = true
+  create_lt = true
+
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags_as_map = {
+    Name = "Web EC2 Server 2"
+  }
+}
