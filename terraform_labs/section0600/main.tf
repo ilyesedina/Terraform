e58855b4ce65750c282784f031877fb82a587b47
Edina Ilyes <edina.ilyes@lego.com>
2024-03-31 11:19:38 +0200

    Consuming S3 Modules form the Terraform Module Registry

--- terraform_labs/section0600/main.tf
+++ terraform_labs/section0600/main.tf
@@ -449,3 +449,15 @@ output "asg_group_size" {
   value = module.autoscaling.autoscaling_group_max_size
 }
 
+# Consuming Modules from the Terraform Module Registry (s3-bucket)
+# https://registry.terraform.io/modules/terraform-aws-modules/s3-bucket/aws/latest?tab=outputs
+
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.11.1"
+}
+
+output "s3_bucket_name" {
+  value = module.s3-bucket.s3_bucket_bucket_domain_name
+}
+
