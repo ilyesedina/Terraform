63accebd3a92db61be8dd844acb448a709a3f280
Edina Ilyes <edina.ilyes@lego.com>
2024-03-31 19:11:39 +0200

    adding an s3 bucket resource -hotfix

--- terraform_labs/section0700/main.tf
+++ terraform_labs/section0700/main.tf
@@ -2,7 +2,7 @@ resource "random_pet" "server" {
   length = 2
 }
 
-/* module "s3-bucket_example_complete" {
-  source  = "terraform-aws-modules/s3-bucket/aws//examples/complete"
-  version = "2.10.0"
-} */
+resource "aws_s3_bucket" "this" {
+  bucket = "qsc-tf-demo-bucket"
+  acl    = "private"
+}
