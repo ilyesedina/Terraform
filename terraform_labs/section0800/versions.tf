26c363547ec86138e39d7d82b86a6dc6994b5945
Edina Ilyes <edina.ilyes@lego.com>
2024-04-12 22:21:59 +0200

    parsal configuration

--- terraform_labs/section0800/versions.tf
+++ terraform_labs/section0800/versions.tf
@@ -30,20 +30,13 @@ terraform {
 }
 
 terraform {
- # Local backend
+/*  # Local backend
   backend "local" {
     path = "mystate/terraform.tfstate"
-  } 
-/*  backend "s3" {
-    bucket = "my-terraform-state-demo-ilyes"
-    // S3 bucket created manually through the AWS Console and referenced here
-    key    = "dev/aws_infra"
-    region = "eu-west-1"
-
-    # DynamoDB table for locking the state file
-    dynamodb_table = "terraform-locks-demo"
-    encrypt        = true
-  }  */ 
+  }  */
+ backend "s3" {
+ 
+  }  
 
   # HTTP backend 
   /* backend "http" {
