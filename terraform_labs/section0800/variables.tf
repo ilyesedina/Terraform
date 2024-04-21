361bd2711b210241edc720114e3dd77a3c6710d6
Edina Ilyes <122867352+dkEdiIly@users.noreply.github.com>
2024-04-21 14:53:24 +0200

    DPQUAL-614/ Eighth Lab (#15)
    * adding changes from prior section(7)
    
    * local backend file reference
    
    * using out of the standard local state backend
    
    * Authentication: Remote Enhanced Backend
    
    * dynamodb_table for s3 to protect state
    
    * http backend state with a python app
    
    * Terraform Cloud backend state
    
    * gitignore
    
    * Saving state in Terraform Cloud
    
    * git ignore sensitive info files
    
    * migrating state to default local
    
    * migrate from standered backend to S3
    
    * migrated to Cloud
    
    * migrating cloud backend to local backend
    
    * Terraform Backend Configuration
    
    * parsal configuration
    
    * Change state backend configuration back to default
    
    * gitignore
    
    * sensitive data protected
    
    * updated gitignore

---
+++ terraform_labs/section0800/variables.tf
@@ -0,0 +1,63 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "demo_vpc"
+}
+
+variable "public_subnets" {
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+  }
+}
+
+variable "variables_sub_cidr" {
+  description = "CIDR Block for the Variables Subnet"
+  type        = string
+  default     = "10.0.202.0/24"
+}
+
+variable "variables_sub_az" {
+  description = "Availability Zone used Variables Subnet"
+  type        = string
+  default     = "eu-west-1a"
+}
+
+variable "variables_sub_auto_ip" {
+  description = "Set Automatic IP Assigment for Variables Subnet"
+  type        = bool
+  default     = "true"
+}
+
+variable "private_subnets" {
+  default = {
+    "private_subnet_1" = 1
+    "private_subnet_2" = 2
+  }
+}
+
+variable "environment" {
+  description = "Environment for deployment"
+  type        = string
+  default     = "dev"
+}
