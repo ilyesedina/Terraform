1abc573ec8f4d3fa516434afdeb4bae7d5d5e095
Edina Ilyes <edina.ilyes@lego.com>
2024-06-07 07:35:28 +0200

    setting output with a data lookup

--- terraform_labs/section0900/outputs.tf
+++ terraform_labs/section0900/outputs.tf
@@ -27,3 +27,16 @@ output "ec2_instance_arn" {
   value     = aws_instance.web_server.arn
   sensitive = true
 }
+
+#Output data lookups
+output "data-bucket-arn" {
+  value = data.aws_s3_bucket.data_bucket.arn
+}
+
+output "data-bucket-domain-name" {
+  value = data.aws_s3_bucket.data_bucket.bucket_domain_name
+}
+
+output "data-bucket-region" {
+  value = "The ${data.aws_s3_bucket.data_bucket.id} bucket is located in ${data.aws_s3_bucket.data_bucket.region}"
+}
