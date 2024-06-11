934df9e4cd00e20e514a5f41a4d4748a3acc5521
Edina Ilyes <edina.ilyes@lego.com>
2024-06-11 07:52:44 +0200

    dynamic block using Terraform var maps

--- terraform_labs/section0900/main.tf
+++ terraform_labs/section0900/main.tf
@@ -217,31 +217,19 @@ resource "aws_subnet" "map_subnet" {
   availability_zone = each.value.az
 }
 
-locals {
-  ingress_rules = [{
-      port        = 443
-      description = "Port 443"
-    },
-    {
-      port        = 80
-      description = "Port 80"
-    }
-  ]
-}
-
 resource "aws_security_group" "main" {
   name   = "core-sg"
   vpc_id = aws_vpc.vpc.id
 
   dynamic "ingress" {
-    for_each = local.ingress_rules
+    for_each = var.web_ingress
 
     content {
       description = ingress.value.description
       from_port   = ingress.value.port
       to_port     = ingress.value.port
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
     }
   }
 }
