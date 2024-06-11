f6ec8ac88e563f6c7167512d608e7988c833bef9
Edina Ilyes <edina.ilyes@lego.com>
2024-06-11 07:36:01 +0200

    Convert Security Group to use dynamic block

--- terraform_labs/section0900/main.tf
+++ terraform_labs/section0900/main.tf
@@ -217,23 +217,31 @@ resource "aws_subnet" "map_subnet" {
   availability_zone = each.value.az
 }
 
+locals {
+  ingress_rules = [{
+      port        = 443
+      description = "Port 443"
+    },
+    {
+      port        = 80
+      description = "Port 80"
+    }
+  ]
+}
+
 resource "aws_security_group" "main" {
   name   = "core-sg"
   vpc_id = aws_vpc.vpc.id
 
-  ingress {
-    description = "Port 443"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  dynamic "ingress" {
+    for_each = local.ingress_rules
 
-  ingress {
-    description = "Port 80"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 }
